36f5392a5254dfd52c683b0fda0e97e29018d68f
이태윤 <tyleeg0901@gmail.com>
2024-09-24 16:22:08 +0900

    feat: 핫토픽 뉴스 데이터 처리 부분 추가
    - 페이징 기능 추가

--- src/components/HotTopics.js
+++ src/components/HotTopics.js
@@ -6,23 +6,52 @@ import {useNavigate} from "react-router-dom";
 const HotTopics = () => {
     const [selectedTopic, setSelectedTopic] = useState(null);
     const [news, setNews] = useState([]);
+    const [page, setPage] = useState(0); // 페이지 상태 추가
+    const [hasMore, setHasMore] = useState(true); // 더 불러올 데이터가 있는지 여부
+    const [loading, setLoading] = useState(false); // 로딩 상태 추가
     const navigate = useNavigate();
 
-    // 토픽 클릭 이벤트 핸들러
-    const handleTopicSelect = (topic) => {
-        setSelectedTopic(topic);
-        axiosClient.get(`/search-by-keyword`, { params: { keyword: topic } })
+    useEffect(() => {
+        if (page > 0) {
+            fetchArticles(page, selectedTopic);
+        }
+    }, [page]);
+
+    const fetchArticles = (pageToFetch, topic) => {
+        setLoading(true);
+        axiosClient.get(`/search-by-keyword?&page=${pageToFetch}&size=12`, { params: { keyword: topic } })
             .then((response) => {
-                setNews(response.data);
+                if (pageToFetch === 0) {
+                    setNews(response.data);
+                } else {
+                    setNews(prevNews => [...prevNews, ...response.data]);
+                }
+                if(response.data.length < 12){
+                    setHasMore(false)
+                }
+                setLoading(false);
             })
             .catch((error) => {
                 console.log("Error fetching news: ", error);
                 setNews([]);
+                setLoading(false);
             });
+    };
+
+    // 토픽 클릭 이벤트 핸들러
+    const handleTopicSelect = (topic) => {
+        setSelectedTopic(topic);
+        setPage(0)
+        setHasMore(true)
+        fetchArticles(page, topic)
     };
 
     const handleArticleClick = (id) => {
         navigate(`/news/${id}`);
+    };
+
+    const loadMoreArticles = () => {
+        setPage(prevPage => prevPage + 1);
     };
 
     return (
@@ -48,6 +77,12 @@ const HotTopics = () => {
                     ))}
                 </div>
             )}
+            {hasMore && !loading && (
+                <div className="load-more-container">
+                    <button onClick={loadMoreArticles}>더보기</button>
+                </div>
+            )}
+            {loading && <p>Loading...</p>} {/* 로딩 중 표시 */}
         </div>
     );
 };
